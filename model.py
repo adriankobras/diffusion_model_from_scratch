Fixed scaling bug in to_vec

model.py
@@ -30,8 +30,8 @@ class ContextUnet(nn.Module):
         self.down1 = UnetDown(n_feat, n_feat)        # down1 #[10, 256, 8, 8]
         self.down2 = UnetDown(n_feat, 2 * n_feat)    # down2 #[10, 256, 4,  4]
         
-         # original: self.to_vec = nn.Sequential(nn.AvgPool2d(7), nn.GELU())
-        self.to_vec = nn.Sequential(nn.AvgPool2d((4)), nn.GELU())
+         # original: self.to_vec = nn.Sequential(nn.AvgPool2d(4), nn.GELU())
+        self.to_vec = nn.Sequential(nn.AvgPool2d((self.h//4)), nn.GELU())
 
         # Embed the timestep and context labels with a one-layer fully connected neural network
         self.timeembed1 = EmbedFC(1, 2*n_feat)